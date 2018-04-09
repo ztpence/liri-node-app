d4f00fb14e773a4b3f8b43e73b02f604d1a35eae
Zane P <ztpence@gmail.com>
2018-04-08 21:22:46 -0700

    setup

--- liri.js
+++ liri.js
@@ -1,1 +1,39 @@
 require("dotenv").config();
+
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
+
+function myTweets(){
+    //show last 20 tweets and when they were created in terminal
+}
+
+function spotify(){
+    //artist
+    //song name
+    //preview link of the song from spotify
+    //the album it's from
+    //if no song is provided it will default to "the sign by Ace of Base"
+}
+
+
+function movie(){
+    //    * Title of the movie.
+    //    * Year the movie came out.
+    //    * IMDB Rating of the movie.
+    //    * Rotten Tomatoes Rating of the movie.
+    //    * Country where the movie was produced.
+    //    * Language of the movie.
+    //    * Plot of the movie.
+    //    * Actors in the movie.
+
+    // If the user doesn't type in a movie it will output the data for Mr. Nobody
+}
+
+function WhatItSays(){
+    //In addition to loggin the data in Bash, output data to a .txt file called log.txt
+    //Make sure to append each command to the log.txt file
+    //don't overwrite each command
+}
