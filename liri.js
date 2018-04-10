ddcce827ec3449c3ddde0e53af3c9c17bbea915f
Zane P <ztpence@gmail.com>
2018-04-09 22:47:45 -0700

    Twitter and Spotify

--- liri.js
+++ liri.js
@@ -1,21 +1,54 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
+var keys = require('./keys.js');
 var twitter = require('twitter');
 var spotify = require('node-spotify-api');
-var request = request('request');
+var request = require('request');
+var fs = require('fs');
 
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var parameters = { name: 'Ztrip2', count:20};
 
 
 
-function myTweets(){
-    //show last 20 tweets and when they were created in terminal
-}
+function MyTweets(){  //show last 20 tweets and when they were created in terminal
+    client.get('statuses/user_timeline',parameters, function(error, tweets, response){
+        if (!error){
+            for (var i = 0; i , tweets.lenghth; i++){
+                console.log('Tweet: ' + tweets[i].text + '\nCreated: ' + tweets[i].created_at);
+            }
+        }
+    });
+};
 
-function spotify(){
+function SpotifySearch(){
+    var spotifyArgs = process.argv;
+    var song = spotifyArgs[3];
+
+    if (song){spotify.search({type: 'track', query: '' + song + ''}, function(err, data){
+        if (err){
+            return console.log('Spotify Error: ' + err);
+        }
+            // console.log('JSON: ', JSON.stringify(data, null));
+            Console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+            consloe.log('Song Name: ' + data.tracks.items[0].name);
+            console.log('Preview: ' + data.tracks.items[0].preview_url);
+            consloe.log('Album: ' + data.tracks.items[0].album.name);
+    });
+    } else { // if no song is enterd the default song is "The Sign"
+         spotify.search({ type: 'track', query: 'the+sign'}, function(err, data) {
+             if (err){
+                 return console.log('Spotify Error: ' + err);
+             }
+             Console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+            consloe.log('Song Name: ' + data.tracks.items[0].name);
+            console.log('Preview: ' + data.tracks.items[3].preview_url);
+            consloe.log('Album: ' + data.tracks.items[0].album.name);
+         });   
+    }
+
     //artist
     //song name
     //preview link of the song from spotify
